5297afe7f0cef31c24c4d011f243e1af3b175626
ejPark43 <pej22200310@handong.ac.kr>
2025-03-07 23:04:04 +0900

    feat: todo input 공간 생성, todo 탭과 vocab 탭에 기본 스타일 지정

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 function Todo() {
-  return <div>Todo</div>;
+  return (
+    <TodoContainer>
+      Todo: <InputTodo />
+    </TodoContainer>
+  );
 }
 
+const TodoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border: 2px solid magenta;
+`;
+
+const InputTodo = styled.input`
+  border: 2px solid blue;
+  outline: none;
+`;
 export default Todo;
--- src/components/Vocab.jsx
+++ src/components/Vocab.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 function Vocab() {
-  return <div>Vocab</div>;
+  return <VocabContainer>Vocab</VocabContainer>;
 }
 
 export default Vocab;
+const VocabContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border: 2px solid magenta;
+`;
