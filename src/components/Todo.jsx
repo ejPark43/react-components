aeb835cc91373fd9cb03fc9ff32fb2502e4f5ac2
ejPark43 <pej22200310@handong.ac.kr>
2025-04-07 23:59:10 +0900

    feat: scroll 기능 추가

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,11 +12,7 @@ import styled from "styled-components";
 // 방금 작성한 todo를 local storage에 저장한다.
 const TODOS_KEY = "todos"; // 로컬 저장소에 저장된 키 이름
 
-let todos = []; //새로운 todo가 생길 때마다 이 배열에 넣어줄 것임.(=push 해줄거임)
-
-function saveTodos() {
-  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
-}
+// [];
 
 // const getTodosFromStorage = () => {
 //   localStorage.getItem(TODOS_KEY);
@@ -28,13 +24,18 @@ function saveTodos() {
 // };
 
 function Todo() {
-  /* localstorage에 이미 저장되어있는 투두스를 가져와서 넣어야함. 현재 새로고침하면 이전에 갖고있던 값을 가져오지 않는중(let 으로 todos를 초기화하는중이라서...) */
-
-  // todos.push(localStorage.getItem("todos"));
   const [value, setValue] = useState("");
+  let todos = [];
+  /* localstorage에 이미 저장되어있는 투두스를 가져와서 넣어야함. 현재 새로고침하면 이전에 갖고있던 값을 가져오지 않는중(let 으로 todos를 초기화하는중이라서...) */
+  todos = JSON.parse(localStorage.getItem("todos")); // localstorage에 들어있던 todos 배열의 값을 가져와서 새로고침했을때도 배열을 동일하게 가져옴
+
+  function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+  }
   const enterTodo = (a) => {
     if (a != "") {
-      todos.push(a);
+      // 빈 문자열이 아닐때만 넣어줌.
+      todos.push(a); //새로운 todo가 생길 때마다 이 배열에 넣어줄 것임.(=push 해줄거임)
       saveTodos();
     }
   };
@@ -42,7 +43,7 @@ function Todo() {
   const enterPressed = useCallback(
     (e) => {
       if (e.code == "Enter") {
-        if (e.nativeEvent.isComposing) return;
+        if (e.nativeEvent.isComposing) return; // 한글 입력 시 마지막글자 중복으로 입력되는 버그 해결. 한글이 끝났을 떄 아직도 현재 composing 중인걸로 착각하기 때문에, 해당 경우 제외 시킴
         console.log("enter pressed!!!");
         let a = e.target.value;
         enterTodo(a);
@@ -52,6 +53,7 @@ function Todo() {
     },
     [value]
   );
+
   /*
   엔터 입력 시: 
   1. 화면에 todo가 새로 추가되어야함. 
@@ -62,6 +64,11 @@ function Todo() {
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+
+  const deleteTodo = (todo) => {
+    console.log("delete ", todo);
+  };
+
   return (
     <TodoContainer>
       <WriteTodo>
@@ -71,26 +78,42 @@ function Todo() {
           onChange={onChange}
           value={value}
         />
-        <SetTodoBtn onClick={() => console.log("set todo!")}>set</SetTodoBtn>
+        {/* <SetTodoBtn onClick={() => console.log("set todo!")}>set</SetTodoBtn> */}
       </WriteTodo>
       <ShowTodos>
         {todos.map((todo) => (
-          <EachTodo>{todo}</EachTodo>
+          <EachTodo key={todo}>
+            {todo} <DeleteBtn onClick={() => deleteTodo(todo)}>X</DeleteBtn>
+          </EachTodo>
         ))}
       </ShowTodos>
     </TodoContainer>
   );
 }
+const DeleteBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1rem;
+  height: 1rem;
+  cursor: pointer;
+  border: 2px solid red;
+`;
 const EachTodo = styled.div`
   display: flex;
-  border: 2px solid red;
+  padding: 8px;
+  width: 50%;
+  justify-content: space-between;
+  border-bottom: 2px solid red;
 `;
 const ShowTodos = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
-  height: 100%;
+  height: 600px;
   border: 2px solid gold;
+  overflow: scroll;
 `;
 const TodoContainer = styled.div`
   display: flex;
@@ -113,15 +136,15 @@ const InputTodo = styled.input`
   width: 200px;
   font-size: 15px;
 `;
-const SetTodoBtn = styled.div`
-  padding: 10px;
-  /* width: 1rem; */
-  border: 2px solid #29b943;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: #29b943;
-    transition: 0.1s;
-  }
-`;
+// const SetTodoBtn = styled.div`
+//   padding: 10px;
+//   /* width: 1rem; */
+//   border: 2px solid #29b943;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: #29b943;
+//     transition: 0.1s;
+//   }
+// `;
 export default Todo;
