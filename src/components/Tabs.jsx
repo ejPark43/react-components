e092953520da49c83b7383d2b72c47539184a3e7
ejPark43 <pej22200310@handong.ac.kr>
2025-04-19 23:29:50 +0900

    style: Tabs 색상변경, 버튼 스타일 컴포넌트 생성

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-
+import { theme } from "../styles/theme";
 function Tabs({ tabs, setTabs }) {
   //   setTabs("TODO");
   return (
     <Container>
-      <button onClick={() => setTabs("todo")}>Todo list</button> {tabs}
-      <button onClick={() => setTabs("vocab")}>Vocab Study</button>
+      <Button onClick={() => setTabs("todo")}>Todo list</Button> {tabs}
+      <Button onClick={() => setTabs("vocab")}>Vocab Study</Button>
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+`;
+const Button = styled.div`
+  display: flex;
+  padding: 8px;
+  /* border: 2px solid blue; */
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${theme.colors.demi_blue};
+  }
 `;
 export default Tabs;
